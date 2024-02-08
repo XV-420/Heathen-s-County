a37a42227aedc55b6236941ada31dff41f8522f8
William Tallarico <wct9667@rit.edu>
2024-02-07 22:34:11 -0500

    made some files, utils, canvas utils

---
+++ src/canvasobject.js
@@ -0,0 +1,21 @@
+import { makeColor } from "./utils";
+
+
+//Basic Object for the canvas class
+class CanvasObject {
+    //takes in canvas size
+    constructor(_xPos = 0, _yPos = 0, _scale = 1, ) {
+      this.position = new Vec(_xPos, _yPos);   
+      this.scale = _scale; //scale = 1 by default
+      this.color = makeColor(255,255,255,255);//black : temp
+      this.size = 1; //makes sure the scale doesn't permanently change the object
+    };
+     
+    //default draw an arc at the objects position
+    draw(ctx) {
+      //will be overridden in the child, for now
+      canvasUtils.drawArc(ctx, this.position.components[0], this.position.components[1], this.size * this.scale,this.color,0,this.color,0, 2 * Math.PI);
+    }
+}
+
+export{CanvasObject}
