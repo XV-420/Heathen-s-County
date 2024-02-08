3a4638ceb2cef83c01a73cf9d4dbd81fa38ea077
William Tallarico <wct9667@rit.edu>
2024-02-07 20:19:34 -0500

    updated the cultist class

--- src/cultist-class.js
+++ src/cultist-class.js
@@ -1,15 +1,14 @@
-
 class Cultist {
     //takes in the passive gen, gen on death, x and y for canvas position
     //kinda a "dumb" class
-    constructor(_passiveFaithGeneration, _faithOnDeath, _xPos, _yPos) {
+    constructor(_passiveFaithGeneration, _faithOnDeath, _passiveCoinDrain, _xPos, _yPos) {
         //set values
         this.passiveFaithGeneration = _passiveFaithGeneration;
         this.faithOnDeath = _faithOnDeath;
+        this.passiveCoinDrain = _passiveCoinDrain;
         this.xPOs = _xPos;
         this.yPos = _yPos;
     };
 }
-
 
 export {Cultist}
