86e1ff4b8c83dc278d581d4537cfb478b5bd906a
William Tallarico <wct9667@rit.edu>
2024-02-11 23:55:08 -0500

    more updates to the class

--- src/shopitem.js
+++ src/shopitem.js
@@ -1,24 +1,50 @@
+import * as resouce from "./resources.js"
+
+const price = {
+    id: 0,
+    name: '',
+    country: '',
+    pets: [],
+}
+
 
 class ShopItem
 {
-    ShopItem(image = null){        
-        let level = 1; //level of the object
-        let price = []; //price to buy intially
-        let upgradePrice = [level * 1]; //price to upgrade
-        let image; //image?
-        
+    ShopItem(image = null, price, cultistManager){        
+        this.level = 1; //level of the object
+        this.price = price; //price to buy intially
+        this.upgradePrice = []; //price to upgrade
+        this.image; //image?
+        this.assignedCultists = 0; 
+        this.cultistManager = cultistManager;   
     };
 
     Upgrade(){
         level++; 
     };
 
+    AssignCultist(amount = amount){
+        this.assignedCultists + amount;
+    };
+
+    RemoveCultist(amount = 1){
+        this.assignedCultists - amount;
+
+        if(this.assignedCultists < 0) this.assignedCultists = 0;
+    }
+
     Update(){};
 }
 
 class Church extends ShopItem
 {
-    Church(){
-        super();
+    Church(image, price, faith){
+        super(image, price);
+        this.faith = faith;
+    }
+
+    Update(){
+        if(this.level > 2)
+            this.cultistManager.GrowFaith();
     }
 }
