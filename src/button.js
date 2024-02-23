00b5e771110d9e88265ca82e0d027c1e6eb65c38
Jolie-rit <jgw4394@rit.edu>
2024-02-22 19:28:45 -0500

    Finished
    finished

--- src/button.js
+++ src/button.js
@@ -1,70 +1,66 @@
 class Button {
+  //takes in the button itself
+  constructor(_button, _loadingTime, onclick) {
+    //set values
+    this.button = _button;
+    this.loadingTime = _loadingTime;
+    this.percent = 0;
+    this.clicked = false;
 
-    //takes in the button itself
-    constructor(_button, _loadingTime, onclick) {
-        //set values
-        this.button = _button;
-        this.loadingTime = _loadingTime;
+    //create progress div
+    this.childName = document.createElement('div');
+    this.childName.classList.add('button-name');
+    this.button.appendChild(this.childName);
+
+    this.child = document.createElement('div');
+    this.child.classList.add('button_progress');
+    this.button.appendChild(this.child);
+
+    //onclick
+    this.button.onclick = () => {
+      if (!this.clicked) {
+        this.clicked = true;
+        onclick();
+      }
+    };
+
+    //update
+    this.update();
+  }
+
+  ChangeName(name) {
+    this.childName.innerHTML = name;
+  }
+
+  //changes the time on the button
+  changeLoadingTime(_loadingTime) {
+    if (this._loadingTime > 0) this.loadingTime = _loadingTime;
+  }
+
+  //add one to percent (will need to scale by delta time )
+  update() {
+    if (this.clicked) {
+      this.percent += this.loadingTime;
+      this.#setProgressBar();
+      if (this.percent >= 100) {
         this.percent = 0;
         this.clicked = false;
-
-        //create progress div
-        this.childName = document.createElement("div");
-        this.childName.classList.add("button-name");
-        this.button.appendChild(this.childName);
-
-        this.child = document.createElement("div");
-        this.child.classList.add("button_progress");
-        this.button.appendChild(this.child);
-
-        //onclick
-        this.button.onclick = () => {
-            if (!this.clicked) {
-                this.clicked = true;
-                onclick();
-            }
-        }
-
-        //update
-        this.update();
-    };
-
-    ChangeName(name){
-        this.childName.innerHTML = name;
+      }
     }
+  }
 
-    //changes the time on the button
-    changeLoadingTime(_loadingTime) {
-        if (this._loadingTime > 0)
-            this.loadingTime = _loadingTime;
-    };
+  Disable() {
+    this.#setProgressBar(0);
+    this.clicked = true;
+  }
+  Enable() {
+    this.#setProgressBar(100);
+    this.clicked = false;
+  }
 
-    //add one to percent (will need to scale by delta time )
-    update() {
-        if (this.clicked) {
-            this.percent += this.loadingTime;
-            this.#setProgressBar();
-            if (this.percent >= 100) {
-                this.percent = 0;
-                this.clicked = false;
-            }
-        }
-    };
-
-    Disable(){
-        this.#setProgressBar(0);
-        this.clicked = true;
-    }
-    Enable(){
-        this.#setProgressBar(100);
-        this.clicked = false;
-    }
-
-    #setProgressBar(percent = this.percent) {
-        this.child.style.width = `${percent}%`;
-    };
-
-
+  #setProgressBar(percent = this.percent) {
+    this.child.style.width = `${percent}%`;
+  }
 }
 
-export { Button }
+export { Button };
--- src/main.js
+++ src/main.js
@@ -1,117 +1,129 @@
+import * as canvas from './canvas.js';
+import { Button } from './button.js';
+import * as resources from './resources.js';
+import * as buildings from './buildings.js';
 
-import * as canvas from "./canvas.js"
-import { Button } from './button.js'
-import * as resources from "./resources.js"
-import * as buildings from "./buildings.js"
-
-let faith = new resources.Faith("Faith", 0, 1);
-let money = new resources.Money("Money", 3000, 1);
-let food = new resources.Food("Food", 3000, 1);
-let cultistManager = new resources.CultistManager("Cultists", faith, food, money);
-
+let faith = new resources.Faith('Faith', 0, 1);
+let money = new resources.Money('Money', 10, 1);
+let food = new resources.Food('Food', 10, 1);
+let cultistManager = new resources.CultistManager(
+  'Cultists',
+  faith,
+  food,
+  money
+);
 
 //last as it takes in faith and cultist and other resources in the future
-const buildingManager = new buildings.BuildingManager(cultistManager, faith, money, food);
+const buildingManager = new buildings.BuildingManager(
+  cultistManager,
+  faith,
+  money,
+  food
+);
 
 const init = () => {
-   let scoreOutput = document.querySelector("#cultists");
-   let faithOutput = document.querySelector("#faith");
-   let foodOutput = document.querySelector("#food");
-   let moneyOutput = document.querySelector("#money");
-   let cultdisplay = document.querySelector("#mainroom");
-   let Shopdisplay = document.querySelector('#testroom');
-   let shopTab = document.querySelector('#test-tab');
-   let culttab = document.querySelector("#cult-tab");
-   let GifRunning = false;
-   let imgheart = document.querySelector('.hvr-pulse-grow');
-   //for now buttons update here
-   const onclickScore = () => {
-      // score++;
-      // scoreOutput.innerHTML = score;
-      cultistManager.AddCultist();
-      scoreOutput.innerHTML = cultistManager.amount;
-   };
+  let scoreOutput = document.querySelector('#cultists');
+  let faithOutput = document.querySelector('#faith');
+  let foodOutput = document.querySelector('#food');
+  let moneyOutput = document.querySelector('#money');
+  let cultdisplay = document.querySelector('#mainroom');
+  let Shopdisplay = document.querySelector('#testroom');
+  let shopTab = document.querySelector('#test-tab');
+  let culttab = document.querySelector('#cult-tab');
+  let GifRunning = false;
+  let imgheart = document.querySelector('.hvr-pulse-grow');
+  //for now buttons update here
+  const onclickScore = () => {
+    // score++;
+    // scoreOutput.innerHTML = score;
+    cultistManager.AddCultist();
+    scoreOutput.innerHTML = cultistManager.amount;
+  };
 
-   const onclickFaith = () => {
-      //sacrifices a cultist
-      cultistManager.SacrificeRandomCultist();
+  const onclickFaith = () => {
+    //sacrifices a cultist
+    cultistManager.SacrificeRandomCultist();
 
-      //increase faith by one, but decrease score by one
-      // faith.amount++;
-      // score--;
-      // //update HTML
-      scoreOutput.innerHTML = cultistManager.amount;
-   }
+    //increase faith by one, but decrease score by one
+    // faith.amount++;
+    // score--;
+    // //update HTML
+    scoreOutput.innerHTML = cultistManager.amount;
+  };
 
-   const onclickPray = () => {
-      faith.amount++;
-   }
+  const onclickPray = () => {
+    faith.amount++;
+  };
 
-   const loop = () => {
-      setTimeout(loop, 1000 / 60);
+  const loop = () => {
+    setTimeout(loop, 1000 / 60);
 
-      //TODO: move this out of main when refactoring
-      //Unlock Recruit when church is level 1 or higher
-      if (buildingManager.CheckChurchLevel() >= 1) {
-         //TODO: Do Enable and Update do the same thing?
-         //recruitButton.Enable();
-         recruitButton.update();
-      }
-      else {
-         recruitButton.Disable();
-      }
-      //TODO: Reconsider unlock levels
-      //unlock sacrifice when church is level 3 or higher
-      if (buildingManager.CheckChurchLevel() >= 3) {
-         sacrificeButton.update();
-      }
-      else {
-         sacrificeButton.Disable();
-      }
+    //TODO: move this out of main when refactoring
+    //Unlock Recruit when church is level 1 or higher
+    if (buildingManager.CheckChurchLevel() >= 1) {
+      //TODO: Do Enable and Update do the same thing?
+      //recruitButton.Enable();
+      recruitButton.update();
+    } else {
+      recruitButton.Disable();
+    }
+    //TODO: Reconsider unlock levels
+    //unlock sacrifice when church is level 3 or higher
+    if (buildingManager.CheckChurchLevel() >= 3) {
+      sacrificeButton.update();
+    } else {
+      sacrificeButton.Disable();
+    }
 
-      prayButton.update();
-      buildingManager.Update();
+    prayButton.update();
+    buildingManager.Update();
 
-      faithOutput.innerHTML = Math.round(faith.amount);
-      scoreOutput.innerHTML = cultistManager.amount;
-      foodOutput.innerHTML = Math.round(food.amount);
-      moneyOutput.innerHTML = Math.round(money.amount);
+    faithOutput.innerHTML = Math.round(faith.amount);
+    scoreOutput.innerHTML = cultistManager.amount;
+    foodOutput.innerHTML = Math.round(food.amount);
+    moneyOutput.innerHTML = Math.round(money.amount);
 
-      if (cultistManager.amount == 0) {
-         imgheart.src = "./assets/heart_static.png";
-         GifRunning = false;
+    if (cultistManager.amount == 0) {
+      //imgheart.src = './assets/heart_static.png';
+      GifRunning = false;
+    } else {
+      if (GifRunning == false) {
+        //imgheart.src = './assets/heartgif.gif';
+        GifRunning = true;
       } else {
-
-         if (GifRunning == false) {
-
-
-            imgheart.src = "./assets/heartgif.gif";
-            GifRunning = true;
-         } else {
-
-         }
-
       }
-   };
-   const onclickCult = () => {
-      cultdisplay.style.display = "block";
-      Shopdisplay.style.display = "none";
+    }
+  };
+  const onclickCult = () => {
+    cultdisplay.style.display = 'block';
+    Shopdisplay.style.display = 'none';
+  };
+  const onclickTest = () => {
+    cultdisplay.style.display = 'none';
+    Shopdisplay.style.display = 'block';
+  };
 
-   }
-   const onclickTest = () => {
-      cultdisplay.style.display = "none";
-      Shopdisplay.style.display = "block";
-   }
+  const recruitButton = new Button(
+    document.querySelector('#recruit-button'),
+    1,
+    onclickScore
+  );
+  const sacrificeButton = new Button(
+    document.querySelector('#faith-button'),
+    5,
+    onclickFaith
+  );
+  const prayButton = new Button(
+    document.querySelector('#pray-button'),
+    500,
+    onclickPray
+  );
 
-   const recruitButton = new Button(document.querySelector("#recruit-button"), 1, onclickScore);
-   const sacrificeButton = new Button(document.querySelector("#faith-button"), 5, onclickFaith);
-   const prayButton = new Button(document.querySelector("#pray-button"), 5, onclickPray);
+  culttab.addEventListener('click', onclickCult);
+  shopTab.addEventListener('click', onclickTest);
+  loop();
 
-   culttab.addEventListener("click", onclickCult);
-   shopTab.addEventListener("click", onclickTest);
-   loop();
-
-   canvas.init();
+  canvas.init();
 };
 
-export { init }
+export { init };
